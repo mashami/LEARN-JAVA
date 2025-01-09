d0682c5695cca2e34a085b728358b066567a23e9
Mashami Pacifique <mashamipaccy04@gmail.com>
2025-01-09 09:33:53 +0200

    feat: adding Section 5

--- SECTION3/Dealership.java
+++ SECTION3/Dealership.java
@@ -1,62 +1,63 @@
 import java.util.Scanner;
 
 public class Dealership {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println(" - Welcome to the Java Dealership");
-        System.out.println(" - Select option 'a' to buy a car");
-        System.out.println(" - Select option 'b' to sell a car");
-        char option = scan.nextLine();
-         
-        switch (option) {
-            case 'a' :
-                System.out.println("Your choose is BUY A CAR");
-                break;
 
-            case 'b' :
-            System.out.println("your Choose is SELL A CAR");
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println(" - Welcome to the Java Dealership");
+    System.out.println(" - Select option 'a' to buy a car");
+    System.out.println(" - Select option 'b' to sell a car");
+    String option = scan.nextLine();
 
-            break;
-        
-            default:
-            System.out.println("Invalid option");
-                break;
-        }
+    switch (option) {
+      case "a":
+        System.out.println("Your choose is BUY A CAR");
+        break;
+      case "b":
+        System.out.println("your Choose is SELL A CAR");
 
-        /******************Part 1******************/
-        System.out.println("you chose option " + option);
-        System.out.println("you chose option " + option);
-        System.out.println("invalid option " + option);
+        break;
+      default:
+        System.out.println("Invalid option");
+        break;
+    }
 
-        /******************Case A******************/
+    /******************Part 1******************/
+    System.out.println("you chose option " + option);
+    System.out.println("you chose option " + option);
+    System.out.println("invalid option " + option);
 
-        System.out.println("What is your budget?");
+    /******************Case A******************/
 
-        // budget greater than or equal to 10000
-            System.out.println("Great! A Nissan Altima is available");
-            System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
-            String insurance = scan.nextLine();
-            System.out.println("\nDo you have a license? Write 'yes' or 'no'");
-            String license = scan.nextLine();
-            System.out.println("What is your credit score?");
-            int creditScore = scan.nextInt();
-            // if they have insurace, a license, and credit score greater than 660
-                System.out.println("Sold! Pleasure doing business with you");
+    System.out.println("What is your budget?");
 
-        // they don't meet budget requirements
-                System.out.println("We're sorry. You are not eligible");
+    // budget greater than or equal to 10000
+    System.out.println("Great! A Nissan Altima is available");
+    System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+    String insurance = scan.nextLine();
+    System.out.println("\nDo you have a license? Write 'yes' or 'no'");
+    String license = scan.nextLine();
+    System.out.println("What is your credit score?");
+    int creditScore = scan.nextInt();
+    // if they have insurace, a license, and credit score greater than 660
+    System.out.println("Sold! Pleasure doing business with you");
 
-        /******************Case B******************/
-        System.out.println("What is your car valued at?");
-        int value = scan.nextInt();
-        System.out.println("What is your selling price?");
-        int price = scan.nextInt();
+    // they don't meet budget requirements
+    System.out.println("We're sorry. You are not eligible");
 
-        // value is greater than price and price is less than 30000
-            System.out.println("We will buy your car. Pleasure doing business with you!");
-        //otherwise
-            System.out.println("Sorry, we're not interested.");
+    /******************Case B******************/
+    System.out.println("What is your car valued at?");
+    int value = scan.nextInt();
+    System.out.println("What is your selling price?");
+    int price = scan.nextInt();
 
-        scan.close();
-        }
+    // value is greater than price and price is less than 30000
+    System.out.println(
+      "We will buy your car. Pleasure doing business with you!"
+    );
+    //otherwise
+    System.out.println("Sorry, we're not interested.");
+
+    scan.close();
+  }
 }
